5572c607244e88de3a459803f7b03d4c21000d1f
rahul10mail <rahul10mail@gmail.com>
2017-05-16 12:52:26 +0530

    first python code

---
+++ first.py
@@ -0,0 +1,12 @@
+list = ["rahul", "kumar"]
+dict = {'name' : 'rahul', 'age' : 21}
+tuple = (1, 2)
+set = {'apple', 'banana', 'orange', 'apple'}
+
+"""
+a = float(input("Enter a float: "))
+b = float(input("Enter a float: "))
+"""
+
+if True:
+	print(2 + 5 * 3)
